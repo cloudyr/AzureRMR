8aa6826889400d14884dd88aff97888c40fcabcf
hong-revo <hongooi@microsoft.com>
2018-05-06 01:50:50 +1000

    api version 2018-05-01 causes problems with resource, change default to 2018-02-01

--- R/AzureSMRbase.R
+++ R/AzureSMRbase.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname)
 {
-    azure_api_version="2018-05-01"
+    azure_api_version="2018-02-01"
     options(azure_api_version=azure_api_version)
     invisible(NULL)
 }
