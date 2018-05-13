199271d2f357f8d38ed269ee8a91ffd0ea18c877
hong-revo <hongooi@microsoft.com>
2018-05-13 10:24:20 +1000

    export utilities

--- R/utils.R
+++ R/utils.R
@@ -1,13 +1,5 @@
-# check that 1) all required names are present; 2) optional names may be present; 3) no other names are present
-validate_object_names <- function(x, required, optional=character(0))
-{
-    valid <- all(required %in% x) && all(x %in% c(required, optional))
-    if(!valid)
-        stop("Invalid object names")
-}
-
-
 # set names on a list of objects, where each object contains its name field(s)
+#' @export
 named_list <- function(lst, name_fields="name")
 {
     lst_names <- sapply(name_fields, function(n) sapply(lst, `[[`, n))
@@ -26,6 +18,7 @@ named_list <- function(lst, name_fields="name")
 
 
 # check if a string appears to be a URL (only https allowed)
+#' @export
 is_url=function(x)
 {
     is.character(x) && length(x) == 1 && grepl("^https://", x)
@@ -33,7 +26,17 @@ is_url=function(x)
 
 
 # TRUE for NULL and length-0 objects
+#' @export
 is_empty <- function(x)
 {
     length(x) == 0
+}
+
+
+# check that 1) all required names are present; 2) optional names may be present; 3) no other names are present
+validate_object_names <- function(x, required, optional=character(0))
+{
+    valid <- all(required %in% x) && all(x %in% c(required, optional))
+    if(!valid)
+        stop("Invalid object names")
 }
