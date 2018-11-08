d20534e5c72776348b56211dc1510acc9b040c53
hong-revo <hongooi@microsoft.com>
2018-11-08 12:30:41 +1100

    bump arm api version

--- R/AzureRMR.R
+++ R/AzureRMR.R
@@ -3,7 +3,7 @@ NULL
 
 .onLoad <- function(libname, pkgname)
 {
-    azure_api_version="2018-02-01"
+    azure_api_version="2018-05-01"
     options(azure_api_version=azure_api_version)
     invisible(NULL)
 }
