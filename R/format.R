76f4d70f29c2f48bbdc09ee99270d45c1e5ebe2d
hong-revo <hongooi@microsoft.com>
2019-02-07 20:46:29 +1100

    rm token printing code

--- R/format.R
+++ R/format.R
@@ -2,43 +2,12 @@
 #'
 #' Miscellaneous functions for printing Azure R6 objects
 #'
-#' @param token An Azure OAuth token.
 #' @param env An R6 object's environment for printing.
 #' @param exclude Objects in `env` to exclude from the printout.
 #'
 #' @details
 #' These functions are utilities to aid in printing Azure R6 objects. They are not meant to be called by the user.
 #'
-#' @rdname format
-#' @export
-format_auth_header <- function(token)
-{
-    stopifnot(is_azure_token(token))
-    expiry <- as.POSIXct(as.numeric(token$credentials$expires_on), origin="1970-01-01")
-    obtained <- expiry - as.numeric(token$credentials$expires_in)
-    resource <- token$credentials$resource
-    tenant <- sub("/.+$", "", httr::parse_url(token$endpoint$access)$path)
-    app <- token$app$key
-
-    auth_type <- if(token$params$client_credentials)
-        "client_credentials"
-    else if(token$params$use_device)
-        "device_code"
-    else if(!is.null(token$params$user_params$username))
-        "resource_owner"
-    else "authorization_code"
-
-    hash <- token$hash()
-
-    paste0("Azure Active Directory token for resource ", resource, "\n",
-           "  Tenant: ", tenant, "\n",
-           "  App ID: ", app, "\n",
-           "  Authentication method: ", auth_type, "\n",
-           "  Token valid from: ", format(obtained, usetz=TRUE), "  to: ", format(expiry, usetz=TRUE), "\n",
-           "  MD5 hash of inputs: ", hash, "\n")
-}
-
-
 #' @rdname format
 #' @export
 format_public_fields <- function(env, exclude=character(0))
