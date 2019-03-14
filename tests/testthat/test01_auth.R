7c0e811f221c39f4fa827276d9073de4a0ba8cdf
Hong Ooi <hongooi@microsoft.com>
2019-03-14 21:16:37 +1100

    fixing for R cmd check

--- tests/testthat/test01_auth.R
+++ tests/testthat/test01_auth.R
@@ -23,38 +23,22 @@ test_that("ARM authentication works",
     expect_true(is_azure_token(az2$token))
 })
 
-test_that("Graph authentication works",
-{
-    az <- az_graph$new(tenant=tenant, app=app, password=password)
-    expect_is(az, "az_graph")
-    expect_true(is_azure_token(az$token))
-
-    creds <- tempfile(fileext=".json")
-    writeLines(jsonlite::toJSON(list(tenant=tenant, app=app, password=password)), creds)
-                        
-    az2 <- az_graph$new(config_file=creds)
-    expect_is(az2, "az_graph")
-    expect_true(is_azure_token(az2$token))
-})
-
 test_that("Login interface works",
 {
-    expect_silent(delete_azure_login(tenant=tenant, confirm=FALSE))
-
     lst <- list_azure_logins()
-    expect_true(is.list(lst) && is_empty(lst[[tenant]]))
+    expect_true(is.list(lst))
 
     az3 <- create_azure_login(tenant=tenant, app=app, password=password)
-    expect_is(az3, "az_client")
+    expect_is(az3, "az_rm")
 
     creds <- tempfile(fileext=".json")
     writeLines(jsonlite::toJSON(list(tenant=tenant, app=app, password=password)), creds)
 
     az4 <- create_azure_login(config_file=creds)
-    expect_is(az4, "az_client")
+    expect_is(az4, "az_rm")
 
     az5 <- get_azure_login(tenant)
-    expect_is(az5, "az_client")
+    expect_is(az5, "az_rm")
 
 })
 
